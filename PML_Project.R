d03c4d468d7cd36f26448c3ce7dbab7fc128e64b
Austin Tidmore <austintidmore@gmail.com>
2015-10-23 16:02:20 -0500

    initial creation

---
+++ PML_Project.R
@@ -0,0 +1,21 @@
+git branch gh-pages
+
+git checkout gh-pages
+
+git push origin gh-pages
+
+in gh-pages branch; add 
+
+touch .nojekyll
+
+git add .nojekyll
+
+git commit -m "created .nojekyll file"
+
+git push origin gh-pages
+
+
+
+to view presentation 
+
+atidmore.github.io/repo/filename.html
